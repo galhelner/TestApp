7bf8c8da651c559a47705bfdae61d3d0a6212790
Ariel Fainshtein <ariel@Ariels-iMac.local>
2025-01-09 19:31:22 +0200

    added activity AnotherActivity and some buttons

--- app/src/main/java/com/example/githubtestapp/MainActivity.java
+++ app/src/main/java/com/example/githubtestapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.githubtestapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNavigate = findViewById(R.id.btnNavigate);
+        btnNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
