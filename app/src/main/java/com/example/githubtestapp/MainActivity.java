df79d868e2f4b59213d6f1489488ec9192306051
galhelner <galhelner@gmail.com>
2025-01-09 18:12:22 +0200

    I deleted all the comments!

--- app/src/main/java/com/example/githubtestapp/MainActivity.java
+++ app/src/main/java/com/example/githubtestapp/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.githubtestapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // this comment is new (=
-        // this should be only inside gal_branch
-        // this is a new comment
     }
 }
